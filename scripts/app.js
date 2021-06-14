34e158e24b7005d62f9531d40ede8814e1f627e1
Xtsen <xts.enn@gmail.com>
2021-06-14 20:32:13 +0200

    loader and other function fix bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
 function loader() {
     document.getElementById('loader').style.transform = "scale(0.5)"
     document.getElementById('loader').style.backgroundColor = "transparent";
-
-    checking()
 }
 function transitionPage(destination) {
     document.getElementById('loader').style.transform = "scale(20)"
@@ -11,24 +9,4 @@ function transitionPage(destination) {
         setTimeout(() => {
             document.location.replace(destination + '.html');
         }, 600);
-}
-function getScroll() {
-    var y = window.scrollY;
-    if (y >= 720) {
-        document.getElementById("navbar").style.backgroundColor = "#222";
-        document.getElementById("titleNavBar").style.color = "#fff";
-        document.getElementById("navBarLink").style.color = "#fff";
-        document.getElementById("navBarLink2").style.color = "#fff";
-        document.getElementById("navBarLink3").style.color = "#fff";
-        document.getElementById("navBarLink4").style.color = "#fff";
-        document.getElementById("navBarLink5").style.color = "#fff";
-    }else {
-        document.getElementById("navbar").style.backgroundColor = "transparent";
-        document.getElementById("titleNavBar").style.color = "#222";
-        document.getElementById("navBarLink").style.color = "#222";
-        document.getElementById("navBarLink2").style.color = "#222";
-        document.getElementById("navBarLink3").style.color = "#222";
-        document.getElementById("navBarLink4").style.color = "#222";
-        document.getElementById("navBarLink5").style.color = "#222";
-    }
 }
