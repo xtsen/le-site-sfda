6bbaba368b84ee328645d036386ff7025c0daf7a
Xtsen <xts.enn@gmail.com>
2021-06-14 20:28:02 +0200

    loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 function loader() {
-    document.getElementById('loader').style.transform = "scale(5)"
-    document.getElementById('loader').style.color = "#222"
+    document.getElementById('loader').style.transform = "scale(0.5)"
+    document.getElementById('loader').style.backgroundColor = "transparent";
+
+    checking()
+}
+function transitionPage(destination) {
+    document.getElementById('loader').style.transform = "scale(20)"
+    document.getElementById('loader').style.backgroundColor = "#222";
+    
+        setTimeout(() => {
+            document.location.replace(destination + '.html');
+        }, 600);
 }
 function getScroll() {
     var y = window.scrollY;
