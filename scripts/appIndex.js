dac039a30603b8c62239e0c3ab98ab003c9d5485
Xtsen <xts.enn@gmail.com>
2021-06-14 04:34:59 +0200

    nav bar anim

--- scripts/appIndex.js
+++ scripts/appIndex.js
@@ -9,7 +9,22 @@ function showGalery() {
 }
 function getScroll() {
     var y = window.scrollY;
-    if (y >= 728) {
+    if (y >= 720) {
         showGalery()
+        document.getElementById("navbar").style.backgroundColor = "#222";
+        document.getElementById("titleNavBar").style.color = "#fff";
+        document.getElementById("navBarLink").style.color = "#fff";
+        document.getElementById("navBarLink2").style.color = "#fff";
+        document.getElementById("navBarLink3").style.color = "#fff";
+        document.getElementById("navBarLink4").style.color = "#fff";
+        document.getElementById("navBarLink5").style.color = "#fff";
+    }else {
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("titleNavBar").style.color = "#222";
+        document.getElementById("navBarLink").style.color = "#222";
+        document.getElementById("navBarLink2").style.color = "#222";
+        document.getElementById("navBarLink3").style.color = "#222";
+        document.getElementById("navBarLink4").style.color = "#222";
+        document.getElementById("navBarLink5").style.color = "#222";
     }
 }
